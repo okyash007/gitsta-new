222527240b1f654d39d20c02e0a1c572d611ccd8
Yash Verma <yashmacbook@Yashs-MacBook-Air.local>
2024-05-11 15:43:27 +0530

    add backend init

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./db/connectDb.js";
+import cors from "cors";
+import { errorMiddleWare } from "./middlewares/errorMiddleWare.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const app = express();
+
+connectDb().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`http://localhost:${process.env.PORT} `);
+  });
+});
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.get("/", (req, res) => {
+  res.json({ status: "giiiii" });
+});
+
+app.use(errorMiddleWare);
