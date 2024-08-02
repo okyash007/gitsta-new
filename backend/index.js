108d485a8f1a99a4dced80ab945f9d5e95991294
Yash <yash.verma@tailboost.ai>
2024-08-03 00:40:41 +0530

    add new cors url

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ connectDb().then(() => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://social.okyash.tech"],
     credentials: true,
   })
 );
